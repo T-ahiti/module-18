93f5b41c1ae013dfe43df2e094db6023028f748d
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-01 00:07:40 +0600

    for added

--- even.js
+++ even.js
@@ -1,6 +1,14 @@
-var number = 0;
-while (number < 10){
+// var number = 0;
+// while (number <= 10){
+    // console.log(number);
+    // number +=2;
+// }
+
+
+// odd
+var number = 1;
+while (number <= 10){
     console.log(number);
-    number++;
+    number +=2;
 }
 
