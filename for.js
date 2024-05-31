3ecf838bad73eaa2c62a45eb10ab6a18133060a6
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-01 00:56:32 +0600

    third commit added

---
+++ for.js
@@ -0,0 +1,14 @@
+// for (var roast =0; roast < 7; roast++){
+//     console.log('roast den')
+// }
+
+// simple version of for loop
+for(var i=0; i<7; i++){
+    console.log(i);
+}
+
+
+for (var i=0; i<10; i++){
+    console.log(i);
+}
+
