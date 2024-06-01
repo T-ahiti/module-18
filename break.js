134935daac7482db462e40052080d9db641a81dc
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-01 19:46:07 +0600

    third commit

--- break.js
+++ break.js
@@ -1,6 +1,15 @@
-for (var i=0; i < 20; i++){
-    console.log(i);
-    if(i > 10){
+// for (var i=0; i < 20; i++){
+    // console.log(i);
+    // if(i > 10){
+        // break;
+    // }
+// }
+
+var roast=0;
+while(roast < 10){
+    console.log('roast den');
+    roast ++;
+    if(roast > 4){
         break;
     }
 }
