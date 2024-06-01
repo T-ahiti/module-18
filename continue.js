6d6c831b987407b372bbc2d6163a958784f09d2b
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-01 19:56:50 +0600

    second commit

--- continue.js
+++ continue.js
@@ -1,10 +1,12 @@
 
-var numbers = [45, 44, 23, 5, 234, 2432, 223]
+var numbers = [45, 44, 23, 5, 234, 2432, 223, 32, 96]
 
+
+// continue means oi number/word ta paile ota skip kore next number/word e jabe
 for (var i=0; i<numbers.length; i++){
-    var number = numbers[i]
+    var number = numbers[i];
     if(number > 100){
          continue;
     }
-    console.log(number)
+    console.log(number);
 }
