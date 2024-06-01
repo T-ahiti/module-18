4a97667034412883e2ac73475a7a916fd5e5c2bc
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-01 18:38:05 +0600

    second commit

--- array-loop.js
+++ array-loop.js
@@ -1,2 +1,9 @@
 // target: display every elements of an array
-var numbers = [45, 56, 29, 43, 23, 92];
+// var numbers = [45, 56, 29, 43, 23, 92];
+// for(var i=0; i<7; i++){
+    // var number = numbers[i];
+    // console.log(number);
+// }
+
+
+
