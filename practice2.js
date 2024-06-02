b55da4324a1b8c0df4aa69b142b97e7a415b6aed
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-02 23:54:11 +0600

    second commit

---
+++ practice2.js
@@ -0,0 +1,13 @@
+var fruits = ['apple', 'banana', 'orange'];
+
+var finding = fruits.indexOf('banana');
+// console.log(finding);
+
+fruits[1] = 'mango';
+// console.log(fruits);
+
+fruits.pop();
+// console.log(fruits);
+
+fruits.push('watermelon');
+console.log(fruits);
