e8a4ec40eb00cf5030b84b1471e7f6e1e427b02b
T-ahiti <zannatultasnimtahiti@gmail.com>
2024-06-06 13:49:11 +0600

    last commit

--- practice3.js
+++ practice3.js
@@ -3,5 +3,20 @@ for (i = 0; i < 39;  i++){
 }
 
 for(i=58; i<99; i++){
-    console.log(i)
+    // console.log(i)
+}
+
+for(i=412; i<458; i+=2){
+    // console.log(i);
+}
+
+for(i=581; i<625; i+=2){
+    // console.log(i);
+}
+
+var subjects = ['bangla', 'english', 'math', 'chem', 'phy', 'lion', 'tiger']
+var i = 0;
+while (i < subjects.length) {
+    // console.log(subjects[i]);
+        i++;
 }
